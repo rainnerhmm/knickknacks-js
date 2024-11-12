981228fa8b6cf05df395ecc93e1429505e0d1ea7
rainner <rainner_hmm@outlook.com>
2024-11-11 20:09:41 -0600

    created project, added basic ideas

---
+++ knickknacks/sketch.js
@@ -0,0 +1,32 @@
+// knickknacks library project
+// Rainn Morphy
+// Nov 7th, 2024
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
+
+function jukebox() {
+  // shuffles music and shows song name and credit, smash bros. style
+}
+
+function jabberbox(){
+  // text boxes, with name and avatar support
+}
+
+function bootup() {
+  // shows small credits screen for yourself, p5js, and (if used) other libraries, unity style
+}
+
+function menubasics() {
+  // support for credits, pause, and other menus, on both title screen, and in game
+}
+
+function boxmenus() {
+  // pause, credits, and anything with text go here
+}
